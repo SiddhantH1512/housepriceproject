ec795bca034ec4fb7b587ffeb204c7b3e966a125
Siddhant <siddhanthardikar6@gmail.com>
2024-02-05 22:36:55 -0600

    Cookiecutter setup

---
+++ Capstone/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a capstone project on predicting the house prices in the Gurgaon area of Delhi based on different attributes',
+    author='Siddhant Hardikar',
+    license='MIT',
+)
